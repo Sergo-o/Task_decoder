59b0498ff534bc66e204f59efcd34262454c51dc
Sergo_Kuri <doker258258@ukr.net>
2023-09-21 22:30:02 +0300

    add class Main, field messages console, method encryption, transcript.

---
+++ src/com/github/Sergo_o/Caesar_code/Main.java
@@ -0,0 +1,23 @@
+package com.github.Sergo_o.Caesar_code;
+
+public class Main {
+
+    static String ACTION_SELECTION = "Приветствую!\nВыберите действие:\n" +
+                                        "1 - Зашифровать текст;\n" +
+                                        "2 - Расшифровать текст.";
+    static String DATA_REQUEST = "Введите данные:\n" +
+                                "путь к файлу, где находится исходной текст;\n" +
+                                "путь к файлу, где нужно сохранить результат работы программы;\n" +
+                                "число - шаг сдвига букв.";
+    public static void main(String[] args) {
+        System.out.println(ACTION_SELECTION);
+
+    }
+    public static void encryption(){
+
+    }
+
+    public static void transcript() {
+
+    }
+}
