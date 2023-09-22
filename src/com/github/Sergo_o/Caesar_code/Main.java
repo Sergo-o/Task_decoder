1a41cb6a878f8312340011421cdf720d1c08e316
Sergo_Kuri <doker258258@ukr.net>
2023-09-22 17:08:25 +0300

    add method pathCheck, field alphabet, checks input data.

--- src/com/github/Sergo_o/Caesar_code/Main.java
+++ src/com/github/Sergo_o/Caesar_code/Main.java
@@ -1,5 +1,9 @@
 package com.github.Sergo_o.Caesar_code;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
 
     static String ACTION_SELECTION = "Приветствую!\nВыберите действие:\n" +
@@ -9,15 +13,39 @@ public class Main {
                                 "путь к файлу, где находится исходной текст;\n" +
                                 "путь к файлу, где нужно сохранить результат работы программы;\n" +
                                 "число - шаг сдвига букв.";
+    static char[] CYRILLIC_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
+
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println(ACTION_SELECTION);
+        int operation = scan.nextInt();
+        while (operation != 1 && operation != 2){
+            System.out.println("Неверное число! Выберите число от 1 до 2.");
+        }
+
+        System.out.println(DATA_REQUEST);
+        Path inputPath = Paths.get(scan.nextLine());
+        Path outputPath = Paths.get(scan.nextLine());
+        int stepCoder = scan.nextInt();
+
+        pathCheck(inputPath);
+        pathCheck(outputPath);
+
+        if(operation == 1){
+            encryption(inputPath,outputPath,stepCoder);
+        }else transcript(inputPath,outputPath,stepCoder);
 
     }
-    public static void encryption(){
+    public static void encryption(Path inputPath, Path outputPath, int stepCoder){
 
     }
 
-    public static void transcript() {
+    public static void transcript(Path inputPath, Path outputPath, int stepCoder) {
+
+    }
+
+    public static void pathCheck (Path path) {
 
     }
 }
