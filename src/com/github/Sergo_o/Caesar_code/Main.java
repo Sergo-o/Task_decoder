a341bae67b04f233f8eb0f895dfd5ce79d790e0b
Sergo_Kuri <doker258258@ukr.net>
2023-09-29 20:51:33 +0300

    add logic in method pathCheck, checks input data.

--- src/com/github/Sergo_o/Caesar_code/Main.java
+++ src/com/github/Sergo_o/Caesar_code/Main.java
@@ -1,5 +1,7 @@
 package com.github.Sergo_o.Caesar_code;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
@@ -7,37 +9,49 @@ import java.util.Scanner;
 public class Main {
 
     static String ACTION_SELECTION = "Приветствую!\nВыберите действие:\n" +
-                                        "1 - Зашифровать текст;\n" +
-                                        "2 - Расшифровать текст.";
+            "1 - Зашифровать текст;\n" +
+            "2 - Расшифровать текст.";
     static String DATA_REQUEST = "Введите данные:\n" +
-                                "путь к файлу, где находится исходной текст;\n" +
-                                "путь к файлу, где нужно сохранить результат работы программы;\n" +
-                                "число - шаг сдвига букв.";
+            "путь к файлу, где находится исходной текст;\n" +
+            "путь к файлу, где нужно сохранить результат работы программы;\n" +
+            "число - шаг сдвига букв (положительное число).";
     static char[] CYRILLIC_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".toCharArray();
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
         System.out.println(ACTION_SELECTION);
         int operation = scan.nextInt();
-        while (operation != 1 && operation != 2){
+        while (operation != 1 && operation != 2) {
             System.out.println("Неверное число! Выберите число от 1 до 2.");
         }
 
         System.out.println(DATA_REQUEST);
-        Path inputPath = Paths.get(scan.nextLine());
-        Path outputPath = Paths.get(scan.nextLine());
+        Path inputPath = pathCheck();
+        try{
+            while (Files.size(inputPath) == 0){
+                System.out.println(inputPath + " - Файл пустой! Введи путь к файлу с данными.");
+                inputPath = pathCheck();
+            }
+        } catch (IOException ioException){
+            System.err.println("Ошибка работы с файлом." + ioException.getMessage());
+        }
+
+        Path outputPath = pathCheck();
+
         int stepCoder = scan.nextInt();
+        while(stepCoder<0){
+            System.out.println("Введите положительное число.");
+            stepCoder = scan.nextInt();
+        }
 
-        pathCheck(inputPath);
-        pathCheck(outputPath);
-
-        if(operation == 1){
-            encryption(inputPath,outputPath,stepCoder);
-        }else transcript(inputPath,outputPath,stepCoder);
+        if (operation == 1) {
+            encryption(inputPath, outputPath, stepCoder);
+        } else transcript(inputPath, outputPath, stepCoder);
 
     }
-    public static void encryption(Path inputPath, Path outputPath, int stepCoder){
+
+    public static void encryption(Path inputPath, Path outputPath, int stepCoder) {
 
     }
 
@@ -45,7 +59,15 @@ public class Main {
 
     }
 
-    public static void pathCheck (Path path) {
-
+    public static Path pathCheck() {
+        Path absolutPath = Paths.get(scan.nextLine()).toAbsolutePath();
+        if (Files.notExists(absolutPath)) {
+            System.out.println(absolutPath + " - Такого файла не существует! Укажите путь к существующему файлу!");
+            absolutPath = Paths.get(scan.nextLine()).toAbsolutePath();
+        } else if (Files.isDirectory(absolutPath)) {
+            System.out.println(absolutPath + " - Вы указали путь к директории! Укажите путь к файлу!");
+            absolutPath = Paths.get(scan.nextLine()).toAbsolutePath();
+        }
+        return absolutPath;
     }
 }
