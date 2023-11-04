cf6cc2b77ee96015e5fd6d78f81ea02aac0cacf0
Sergo_Kuri <doker258258@ukr.net>
2023-11-04 22:15:25 +0200

    Работа над decryptionBySelection.

--- src/com/github/Sergo_o/Caesar_code/Main.java
+++ src/com/github/Sergo_o/Caesar_code/Main.java
@@ -1,5 +1,6 @@
 package com.github.Sergo_o.Caesar_code;
 
+import javax.print.DocFlavor;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
@@ -7,17 +8,18 @@ import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     static String ACTION_SELECTION = "Приветствую!\nВыберите действие:\n" +
             "1 - Зашифровать текст;\n" +
-            "2 - Расшифровать текст.";
+            "2 - Расшифровать текст;\n" +
+            "3 - Подбор ключа для расшифровки.";
     static String DATA_REQUEST = "Введите данные:\n" +
             "путь к файлу, где находится исходной текст;\n" +
-            "путь к файлу, где нужно сохранить результат работы программы;\n" +
-            "число - шаг сдвига букв.";
+            "путь к файлу, где нужно сохранить результат работы программы;\n";
+    static String NUMBER_STEP_CODER = "число - шаг сдвига букв.";
     static String CYRILLIC_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // 33 буквы.
     static String LATIN_ALPHABET = "abcdefghijklmnopqrstuvwxyz"; // 26 букв.
     static Scanner scan = new Scanner(System.in);
@@ -26,12 +28,15 @@ public class Main {
 
         System.out.println(ACTION_SELECTION);
         int operation = scan.nextInt();
-        while (operation != 1 && operation != 2) {
-            System.out.println("Не верно выбрана операция, введите число 1 или 2!");
+        while (operation != 1 && operation != 2 && operation != 3) {
+            System.out.println("Не верно выбрана операция, введите число от 1 до 3!");
             operation = scan.nextInt();
         }
 
+        if (operation == 3){
         System.out.println(DATA_REQUEST);
+        }else System.out.println(DATA_REQUEST + NUMBER_STEP_CODER);
+
         String inputPath = pathCheck();
         try{
             while (Files.size(Path.of(inputPath)) == 0){
@@ -44,13 +49,108 @@ public class Main {
 
         String outputPath = pathCheck();
 
-        int stepCoder = scan.nextInt();
+        int stepCoder = 0;
+        if(operation != 3){
+            stepCoder = scan.nextInt();}
 
         switch (operation){
             case 1 -> encryption(inputPath,outputPath,stepCoder);
             case 2 -> transcript(inputPath,outputPath,stepCoder);
+            case 3 -> decryptionBySelection(inputPath,outputPath);
         }
 
+    }
+
+    public static void decryptionBySelection(String inputPath, String outputPath){
+        String TOP_RUSSIAN_WORDS = "и,в,не,на,я,быть,он,с,что,а,по,это,она,этот,к,но,они,мы,как,из,у,который,то,за," +
+                "свой,что,весь,год,от,так,о,для,ты,же,все,тот,мочь,вы,человек,такой,его,сказать,только,или,ещё,бы," +
+                "себя,один,как,уже,до,время,если,сам,когда,другой,вот,говорить,наш,мой,знать,стать,при,чтобы,дело," +
+                "жизнь,кто,первый,очень,два,день,её,новый,рука,даже,во,со,раз,где,там,под,можно,ну,какой,после,их," +
+                "работа,без,самый,потом,надо,хотеть,ли,слово,идти,большой,должен,место,иметь,ничто";
+        String[] words = TOP_RUSSIAN_WORDS.split(",");
+        int maxLengthWord = 0;
+        Map<String,Integer> TOP_RUSSIAN_WORDS_MAP = new HashMap<>();
+
+        for (String word : words) {
+            TOP_RUSSIAN_WORDS_MAP.put(word,0);
+            if(maxLengthWord<word.length()){
+                maxLengthWord = word.length();
+            }
+        }
+        StringBuffer stringBufferInputData = new StringBuffer();
+
+        try (RandomAccessFile inputFile = new RandomAccessFile(inputPath,"rw");
+                FileChannel inputChannel = inputFile.getChannel()){
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            CharBuffer charBuffer;
+
+            while (inputChannel.read(byteBuffer) != -1){
+                byteBuffer.flip();
+                charBuffer = StandardCharsets.UTF_8.decode(byteBuffer);
+                stringBufferInputData.append(charBuffer);
+
+                byteBuffer.clear();
+            }
+
+        }catch (IOException e){
+            System.err.println("Ошибка работы с файлами данных!" + e.getMessage());
+        }
+
+            String stringInputData = stringBufferInputData.toString();
+            String[] arrayInputData = stringInputData.replaceAll("\\p{Punct}","").split(" ");
+            ArrayList<String> listInputData = new ArrayList<>();
+
+            for (int i = 0; i < arrayInputData.length; i++) {
+                arrayInputData[i] = arrayInputData[i].trim();
+                if(arrayInputData[i].length() <= maxLengthWord){
+                    listInputData.add(arrayInputData[i]);
+                }
+            }
+
+            boolean bool = true;
+            int[] numberOfMatches = new int[CYRILLIC_ALPHABET.length()];
+        while (bool) {
+            ArrayList<String> listOutputData = new ArrayList<>();
+            for (int i = 1; i <= CYRILLIC_ALPHABET.length(); i++) {
+                for (String wordInput : listInputData) {
+                    char[] charsWord = wordInput.toCharArray();
+                    for (int j = 0; j < charsWord.length; j++) {
+                        if(Character.isLetter(charsWord[j])){
+                        charsWord[j] = characterOffsetForward(charsWord[j],i);
+                        }
+                    }
+                    listOutputData.add(Arrays.toString(charsWord));
+                }
+                for (Map.Entry<String,Integer> topRussianWord : TOP_RUSSIAN_WORDS_MAP.entrySet()) {
+                    for (int j = 0; j < listOutputData.size(); j++) {
+                        if(topRussianWord.getKey().equalsIgnoreCase(listOutputData.get(j))){
+                            topRussianWord.setValue(topRussianWord.getValue()+1);
+                        }
+                    }
+                }
+                int sumMatches = 0;
+                for (Map.Entry<String,Integer> topRussianWord : TOP_RUSSIAN_WORDS_MAP.entrySet()) {
+                    sumMatches += topRussianWord.getValue();
+                }
+                numberOfMatches[i-1] = sumMatches;
+                if(i==CYRILLIC_ALPHABET.length()){
+                    bool = false;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(numberOfMatches));
+
+        int maxMatches = 0;
+        int stepCoder = 0;
+
+        for (int i = 0; i < numberOfMatches.length; i++) {
+            if(maxMatches<numberOfMatches[i]){
+                maxMatches = numberOfMatches [i];
+                stepCoder = i;
+            }
+        }
+        System.out.println(stepCoder);
+        transcript(inputPath,outputPath,stepCoder);
     }
 
     public static String alphabetDefinition (char inputChar){
